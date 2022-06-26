ca2a260a5245c0b6389f582d88b8533a6d1436d9
MMFH V.3 <baruta9898gmail.com>
2022-06-26 23:15:14 +0300

    Main page update

--- src/Pages/WelcomeScreen.js
+++ src/Pages/WelcomeScreen.js
@@ -1,16 +1,31 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 import Button from '../components/Button';
 
 function Welcome() {
     return (
-        <View>
-            <Text>Welcome</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}>Welcome</Text>
 <Button text="Uye Kaydi Olustur" onPress={null}/>
         </View>);
 }
 
 export default Welcome;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        textAlign: 'center',
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#e040fb',
+        margin: 10,
+    },
+});
 
 
